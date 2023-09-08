f21686b9ae0b8691c97907186798cac91cbb71dc
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-08 20:43:05 +0300

    Seeds are done.

--- NLayer.Repository/AppDbContext.cs
+++ NLayer.Repository/AppDbContext.cs
@@ -23,6 +23,23 @@ namespace NLayer.Repository
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            modelBuilder.Entity<ProductFeature>().HasData(new ProductFeature()
+            {
+                Id = 1,
+                ProductId = 3,
+                Color = "Red",
+                Height = 10,
+                Width = 20,
+            }, new ProductFeature
+            {
+                Id = 1,
+                ProductId = 2,
+                Color = "Blue",
+                Height = 15,
+                Width = 25,
+            });
+
             base.OnModelCreating(modelBuilder);
         }
 
---
+++ NLayer.Repository/Seeds/ProductSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NLayer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Seeds
+{
+    public class ProductSeed : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasData(new Product
+            {
+                Id = 1,
+                Name =  "Faber Castel 2B",
+                Stock = 100,
+                Price = 12,
+                CategoryId = 1,
+                CreatedDate = DateTime.Now,
+            }, new Product
+            {
+                Id = 2,
+                Name = "Faber Castel 3B",
+                Stock = 75,
+                Price = 15,
+                CategoryId = 1,
+                CreatedDate = DateTime.Now,
+            }, new Product
+            {
+                Id= 3,
+                Name = "Book 1",
+                Stock = 50,
+                Price = 20,
+                CategoryId = 2,
+                CreatedDate = DateTime.Now,
+            }, new Product
+            {
+                Id = 4,
+                Name = "Book 2",
+                Stock = 25,
+                Price = 25,
+                CategoryId = 2,
+                CreatedDate = DateTime.Now,
+            });
+        }
+    }
+}
