7c005491d1bc91811f7a64ce202ef74008225ad0
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-09 23:11:53 +0300

    sub-commit

---
+++ NLayer.API/Controllers/ProductController.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.DTOs;
+using NLayer.Core.Models;
+using NLayer.Core.Services;
+
+namespace NLayer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : CustomBaseController
+    {
+        private readonly IMapper _mapper;
+        private readonly IService<Product> _service;
+        
+        public ProductController(IService<Product> service, IMapper mapper)
+        {
+            _service = service;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProducts()
+        {
+            var products = await _service.GetAllAsync();
+            var productsDto = _mapper.Map<List<ProductDto>>(products.ToList());
+            return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(productsDto,200));
+
+        }
+    }
+}
