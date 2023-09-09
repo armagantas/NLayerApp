f73470aa1693712fd9ea53ff636337d670461398
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-09 20:17:50 +0300

    Service class done.

--- NLayer.Core/Services/IService.cs
+++ NLayer.Core/Services/IService.cs
@@ -12,9 +12,9 @@ namespace NLayer.Core.Services
         Task<T> GetByIdAsync(int id);
         Task<IEnumerable<T>> GetAllAsync();
         IQueryable<T> Where(Expression<Func<T, bool>> expression);
-        Task AddAsync(T entity);
+        Task<T> AddAsync(T entity);
         Task<bool> AnyAsync(Expression<Func<T, bool>> expression);
-        Task AddRangeAsync(IEnumerable<T> entities);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
         Task UpdateAsync(T entity);
         Task RemoveAsync(T entity);
         Task RemoveRangeAsync(IEnumerable<T> entities);
