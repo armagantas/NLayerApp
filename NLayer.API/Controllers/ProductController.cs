386e94fe2f2d84c8333c251aa7be6fe1f713f78b
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-10 03:40:19 +0300

    ProductController done.

--- NLayer.API/Controllers/ProductController.cs
+++ NLayer.API/Controllers/ProductController.cs
@@ -28,5 +28,40 @@ namespace NLayer.API.Controllers
             return CreateActionResult(CustomResponseDto<List<ProductDto>>.Success(productsDto,200));
 
         }
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+            var productDto = _mapper.Map<ProductDto>(product);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(productDto, 200));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(ProductDto productDto)
+        {
+            var productToAdd = await _service.AddAsync(_mapper.Map<Product>(productDto));
+            var productsDto = _mapper.Map<ProductDto>(productToAdd);
+            return CreateActionResult(CustomResponseDto<ProductDto>.Success(productsDto, 201));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductUpdateDto productDto)
+        {
+            await _service.UpdateAsync(_mapper.Map<Product>(productDto));
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(200));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _service.GetByIdAsync(id);
+
+            await _service.RemoveAsync(product);
+            
+            return CreateActionResult(CustomResponseDto<NoContentDto>.Success(204));
+
+        }
     }
 }
