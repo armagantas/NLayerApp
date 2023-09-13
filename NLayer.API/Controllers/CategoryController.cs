7773b0a689ce077b60b2427c5e43b4bd5733cb83
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-13 17:37:08 +0300

    GetCategoryByIdWithProducts Endpoint done.

---
+++ NLayer.API/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using NLayer.Core.Services;
+using System.Runtime.CompilerServices;
+
+namespace NLayer.API.Controllers
+{
+    public class CategoryController : CustomBaseController
+    {
+        private readonly ICategoryService _categoryService;
+        public CategoryController(ICategoryService categoryService) 
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet("[action]/{categoryId}")] // api/category/GetSingleCategoryByIdWithProducts/1
+        public async Task<IActionResult> GetSingleCategoryByIdWithProducts(int categoryId)
+        {
+            return CreateActionResult(await _categoryService.GetSingleCategoryWithProductsAsync(categoryId));
+        }
+    }
+}
