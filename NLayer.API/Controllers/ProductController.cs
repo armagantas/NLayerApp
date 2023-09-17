7bee509655ae972b1c390b69d88ce8f38ba95957
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-18 00:10:29 +0300

    Caching Integration has been done.

--- NLayer.API/Controllers/ProductController.cs
+++ NLayer.API/Controllers/ProductController.cs
@@ -14,7 +14,7 @@ namespace NLayer.API.Controllers
         private readonly IMapper _mapper;
         private readonly IProductService _service;
         
-        public ProductController(IService<Product> service, IMapper mapper, IProductService productService) 
+        public ProductController(IMapper mapper, IProductService productService) 
         {
             
             _mapper = mapper;
--- NLayer.Caching/ProductServiceWithCaching.cs
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -34,7 +34,7 @@ namespace NLayer.Caching
 
             if(!_memoryCache.TryGetValue(CacheProductKey, out _))
             {
-                _memoryCache.Set(CacheProductKey, _repository.GetProductsWithCategory());
+                _memoryCache.Set(CacheProductKey, _repository.GetProductsWithCategory().Result);
             }
         }
         public async Task<Product> AddAsync(Product entity)
