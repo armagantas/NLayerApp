b9cc1bff8f50392eb983ceef03426d1a2e7f7426
Armağan Taş <65099385+armagantas@users.noreply.github.com>
2023-09-19 00:26:34 +0300

    Initial commit.

--- NLayer.Caching/ProductServiceWithCaching.cs
+++ NLayer.Caching/ProductServiceWithCaching.cs
@@ -76,13 +76,13 @@ namespace NLayer.Caching
             return Task.FromResult(product);
         }
 
-        public Task<CustomResponseDto<List<ProductWithCategoryDto>>> GetProductsWithCategory()
+        public Task<List<ProductWithCategoryDto>> GetProductsWithCategory()
         {
             var products = _memoryCache.Get<IEnumerable<Product>>(CacheProductKey);
 
             var productsWithCategoryDto = _mapper.Map<List<ProductWithCategoryDto>>(products);
 
-            return Task.FromResult(CustomResponseDto<List<ProductWithCategoryDto>>.Success(productsWithCategoryDto, 200));   
+            return Task.FromResult(productsWithCategoryDto);   
         }
 
         public async Task RemoveAsync(Product entity)
---
+++ NLayer.Web/Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NLayer.Core.Services;
+
+namespace NLayer.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+
+        public ProductController(IProductService productService)
+        {
+            _productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {  
+            return View(await _productService.GetProductsWithCategory()); 
+        }
+    }
+}
